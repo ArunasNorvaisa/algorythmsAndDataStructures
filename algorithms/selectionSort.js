efd5130a30a08006379822b8716e5eb89274d916
Arunas Norvaisa <an@86.lt>
2022-11-16 13:50:21 +0200

    selection sort explanation

--- algorithms/selectionSort.js
+++ algorithms/selectionSort.js
@@ -1,3 +1,12 @@
+/*
+In computer science, selection sort is an in-place comparison sorting algorithm. It has an O(n2) time complexity,
+which makes it inefficient on large lists, and generally performs worse than the similar insertion sort. Selection sort
+is noted for its simplicity and has performance advantages over more complicated algorithms in certain situations,
+particularly where auxiliary memory is limited.
+
+https://en.wikipedia.org/wiki/Selection_sort
+*/
+
 const selectionSort = (arr) => {
   let min;
   let tempArray = [...arr];
